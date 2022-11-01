0c4030b8f306bccf0a4b4c16c36b97fe39d099e0
GBfds <95057840+GBfds@users.noreply.github.com>
2022-11-01 17:14:53 -0300

    prototipo do home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-
+import styles from "../styles/home.module.scss";
 
 export default function Home() {
   return (
-    <div>
-      <h1>teste</h1>
+    <div className={styles.homeContent}>
+      <h1>Esse é  um projeto de portifolio</h1>
+      <h2>Um projeto simples usando <span>NextJS</span>, ele se conecta com o banco de dados não relacional <span>FireBase </span> para salvar suas notas online</h2>
+      <h3>Ainda é um projeto simples mas aos poucos irei o melhorando...</h3>
     </div>
   )
 }
---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./styles.module.scss";
+
+export default function Sobre(){
+    return(
+        <h1>sobre</h1>
+    )
+}
