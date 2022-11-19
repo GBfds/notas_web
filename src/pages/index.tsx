600dcaddaf2487a549a9c5dfe135c4c80736cba8
GBfds <95057840+GBfds@users.noreply.github.com>
2022-11-19 10:25:10 -0300

    .

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <title>Home - notas web</title>
     </Head>
     <div className={styles.homeContent}>
-      <h1>Esse é  um projeto de portifolio</h1>
+      <h1>Esse é  um projeto de portfólio</h1>
       <h2>Um projeto simples usando <span>NextJS</span>, ele se conecta com o banco de dados não relacional <span>FireBase </span> para salvar suas notas online</h2>
     </div>
     </>
