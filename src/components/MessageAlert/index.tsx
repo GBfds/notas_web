b1ce44dd28f0a265f3433ba9ea846985e224bf6e
GBfds <95057840+GBfds@users.noreply.github.com>
2023-02-27 19:17:40 -0300

    Revert "."
    This reverts commit 0090988debe68ffee2ca84c187dcdad91784a483.

--- src/components/MessageAlert/index.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./styles.module.scss"
-
-export function MessageAlert(){
-    return(
-        <div className={styles.alert}>
-            <span>A página está em manutenção e pode apresentar mal funcionamento</span>
-        </div>
-    )
-}
