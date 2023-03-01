7ecbd1280026e439fb48ef6a35f0fffeeb451db6
GBfds <95057840+GBfds@users.noreply.github.com>
2023-03-01 17:35:10 -0300

    apdate alert

--- src/components/MessageAlert/index.tsx
+++ src/components/MessageAlert/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss"
 export function MessageAlert(){
     return(
         <div className={styles.alert}>
-            <span>A página está em manutenção e pode apresentar mal funcionamento</span>
+            <span>A página está em desenvolvimento e pode apresentar mal funcionamento</span>
         </div>
     )
 }
